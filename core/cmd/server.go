db47c50bced357b4f8dc63a833b61fdd76997f13
lif <649505671@qq.com>
2025-08-29 17:05:16 +0800

    不用啦，调试结束了

--- core/cmd/server.go
+++ core/cmd/server.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/crawlab-team/crawlab/core/apps"
 	"github.com/crawlab-team/crawlab/core/utils"
 	"github.com/spf13/cobra"
@@ -23,9 +20,9 @@ var serverCmd = &cobra.Command{
 		if !utils.IsPro() {
 			utils.PrintLogoWithWelcomeInfo()
 		}
-		fmt.Println("暂停启动用来接入dlv调试")
-		time.Sleep(time.Minute)
-		fmt.Println("开始启动server啦")
+		//fmt.Println("暂停启动用来接入dlv调试")
+		//time.Sleep(time.Minute)
+		//fmt.Println("开始启动server啦")
 		// app
 		svr := apps.GetServer()
 
